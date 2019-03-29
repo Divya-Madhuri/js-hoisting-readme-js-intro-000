6549d4e811d180155a50e73a79e96596b35b6db9
Divya <alaganduladivyamadhuri@gmail.com>
2019-03-29 16:28:54 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -23,12 +23,13 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-sayMy();
+
 
   function sayMy() {
     var name = "Kristin";
     console.log(name);
 
   }
+  sayMy();
 
 }
